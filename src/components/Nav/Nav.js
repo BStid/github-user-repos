70d04216090126dd8c9cb6dc634472797ba77b75
Benjamin Stidham <benjamindstidham@gmail.com>
2019-02-16 17:48:01 -0600

    start building

---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Nav.css";
+
+export default function Nav() {
+  return (
+    <div className="navContainer">
+      <h3>Github Browser</h3>
+    </div>
+  );
+}
