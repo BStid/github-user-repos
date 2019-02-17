b4697f3c0759299a0f0bc50574873fa57660ac7b
Benjamin Stidham <benjamindstidham@gmail.com>
2019-02-17 02:07:10 -0600

    added clickable link to repo

--- src/components/DisplayRepos/DisplayRepos.js
+++ src/components/DisplayRepos/DisplayRepos.js
@@ -4,12 +4,20 @@ import "./DisplayRepos.css";
 export default function DisplayRepos(props) {
   let renderRepos = props.repositories.map((repo, index) => {
     return index < 12 ? (
-      <div className="repoContainer" key={index}>
+      <div
+        className="repoContainer"
+        key={index}
+        onClick={() => window.open(repo.html_url, "_blank")}
+      >
         <div className="repoName">{repo.name}</div>
         <div className="repoBody">
           {repo.description
             ? repo.description
             : "Oops! This repository does not have a description!"}
+        </div>
+        <div className="repoLanguage">
+          Language: <br />{" "}
+          {repo.language ? repo.language : "No Language Detected!"}
         </div>
       </div>
     ) : null;
